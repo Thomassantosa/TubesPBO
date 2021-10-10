8bf09af717b98655d5d5fdb430d3a63b387e1de0
renaldtjandraa <nyxargentum16@gmail.com>
2021-10-10 20:35:14 +0700

    edit main

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,6 @@ import model.User;
 public class Main extends User {
     public static void main(String[] args) {
         System.out.println("TEST");
-        System.out.println("halo");
+        
     }
 }
