da7d8ec0b1903ad58c2ab9c0ad6d3913eaab98b2
JoFarrell19 <j.farrelllewi@gmail.com>
2021-10-10 20:49:52 +0700

    parel

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ public class Main extends User {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("halo");
+        System.out.println("Parel");
     }
 }
